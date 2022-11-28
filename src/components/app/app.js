ff92f6331cff33b826e96fb8fe16071de6a328ee
Artem_Ti <Art.Tikhonenko@gmail.com>
2022-11-28 09:40:43 +0300

    Filters

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,7 +17,8 @@ class App extends Component {
         {name: 'Alex M.', salary: 3000, increase: true, rise: false, id: 2},
         {name: 'Brayan O.', salary: 55000, increase: false, rise: false, id: 3},
       ],
-      term: ''
+      term: '',
+      filter: 'all'
     }
     this.maxId = 4;
   }
@@ -82,11 +83,26 @@ onUpdateSearch = (term) => {
   this.setState({term});
 }
 
+filterPost = (items, filter) => {
+  switch (filter) {
+    case 'rise': 
+      return items.filter(item => item.rise);
+    case 'moreThen100':
+      return items.filter(item => item.salary);
+    default:
+      return items
+  }
+}
+
+onFilterSelect = (filter) => {
+  this.setState({filter});
+}
+
   render() {
-    const {data, term} = this.state;
+    const {data, term, filter} = this.state;
     const employees = this.state.data.length;
     const increased = this.state.data.filter(item => item.increase).length;
-    const visibleData = this.searchEmp(data, term);
+    const visibleData = this.filterPost(this.searchEmp(data, term), filter);
 
     return (
       <div className="app">
@@ -94,7 +110,7 @@ onUpdateSearch = (term) => {
   
           <div className="search-panel">
               <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
-              <AppFilter/>
+              <AppFilter filter={filter} onFilterSelect={this.onFilterSelect}/>
           </div>
           
           <EmployeesList 
